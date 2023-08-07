6c7300968138d54d670d2c464aeb6e6f331b197b
1willcobb <cobb.will@gmail.com>
2023-08-07 06:57:06 -0700

    models complete

---
+++ models/Blog.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection')
+
+class Blog extends model {}
+
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        text: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        author_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                mode: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog',
+    }
+);
+
+module.exports = Blog;
