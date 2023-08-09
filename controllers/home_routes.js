afa33cfff1c529978d4fc917fc5a4a0eb3fccb42
1willcobb <cobb.will@gmail.com>
2023-08-09 07:30:20 -0700

    still writing logic for login, need to add login logic

--- controllers/home_routes.js
+++ controllers/home_routes.js
@@ -1,33 +1,33 @@
-const router = require('express').Router();
-const { Blog } = require('../models');
+const router = require("express").Router();
+const { Blog } = require("../models");
 
-router.get('/', async (req, res) => {
-    try {
-        res.render('homepage')
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error)
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    //TODO -> add algorithmic way to display top 3 posts OR change this view every 10 seconds?
 
-router.get('/feed', async (req, res) => {
-    try {
-        res.render('/feed')
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error)
-    }
-})
+    res.render("homepage");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
 
-router.get('/login', async (req, res) => {
-    try {
-        res.render('/login')
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error)
-    }
-})
+router.get("/feed", async (req, res) => {
+  try {
+    res.render("feed");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
 
-
+router.get("/login", async (req, res) => {
+  try {
+    res.render("/login");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
