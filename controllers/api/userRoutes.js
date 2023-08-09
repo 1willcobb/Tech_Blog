cb39decd41c8ab3ad82809419a08315198d95911
1willcobb <cobb.will@gmail.com>
2023-08-09 07:12:53 -0700

    added post login route in user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -16,6 +16,38 @@ router.post('/', async (req, res) => {
       }
 })
 
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    });
+
+    if (!userData){
+      res.status(400).json({message: "Incorrect email or password, please try again"});
+    }
+
+    const validatePassword = await userData.checkPassword(req.body.password);
+
+    if (!validatePassword) {
+      res.status(400).json({message: "Incorrect email or password, please try again"});
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+    
+
+    res.json({ message: 'You are Logged In!', user: userData})
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error)
+  }
+});
+
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(()=>{
