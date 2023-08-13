8177464deaa87d47c25d1183f1a0f2762778fb60
1willcobb <cobb.will@gmail.com>
2023-08-13 14:56:18 -0700

    new models for comments, index.js mnodels, and new seeds successfully uploading

--- models/Comment.js
+++ models/Comment.js
@@ -1,19 +1,15 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Blog extends Model {}
+class Comment extends Model {}
 
-Blog.init(
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
     },
     text: {
       type: DataTypes.TEXT,
@@ -22,6 +18,14 @@ Blog.init(
     is_editing: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'blog',
+        key: 'id',
+      },
     },
     author_id: {
       type: DataTypes.INTEGER,
@@ -36,8 +40,8 @@ Blog.init(
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'blog',
+    modelName: 'comment',
   },
 );
 
-module.exports = Blog;
+module.exports = Comment;
